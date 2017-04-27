771135c06e6cff8efbc036b8ed46e78b9dd81db6
lvdi <lvdi@kuaiyd.com>
2017-04-27 16:35:18 +0800

    plus init

---
+++ plus-pojo/src/main/java/net/lvddy/App.java
@@ -0,0 +1,13 @@
+package net.lvddy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
